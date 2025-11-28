30ac8f0fd71aadf89903a486fa707775b3c94189
Kanishk Singh Maurya <95866249+kanishk466@users.noreply.github.com>
2025-11-28 02:49:19 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
-console.log(process.env.GEMINI_API_KEY)
 const recordBtn = document.getElementById("record-btn");
 
 const pulseRing = document.getElementById("pulse-ring");
@@ -263,6 +262,7 @@ outputLang.onchange = async () => {
 updateStatus("ready", "Ready to record");
 
 })
+
 
 
 
